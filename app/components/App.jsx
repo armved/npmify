e9ed4866174c6133d9c5b9bd670dd5367989896e
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-07-24 18:16:42 +0300

    Basic structure

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import PkgList from './PkgList';
+import Search from './Search';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <MuiThemeProvider>
+    <div style={{ width: 1024, margin: '0 auto' }}>
+      <h1>App</h1>
+      <Search />
+      <PkgList />
+    </div>
+  </MuiThemeProvider>
 );
 
 export default App;
---
+++ app/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+const Search = () => (
+  <div>
+    <TextField hintText="Search" />
+  </div>
+);
+
+export default Search;
